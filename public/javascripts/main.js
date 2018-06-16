a0e57f80d76e9b1bfc750d0e83eddbdb8ea81226
Breno Silva <brenosilva1@gmail.com>
2018-06-16 20:47:42 -0300

    change add form function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,26 @@
-let buttonAdd = document.querySelector('.add-form');
+// let buttonAdd = document.querySelector('.add-form');
 let form = document.querySelector('.dj');
 let count = 0;
 
-buttonAdd.addEventListener('click', () => {
-    count += 1;
+let periods = document.querySelector('.periods');
+periods.addEventListener('change',  function(){
+	count = Number(this.value);
+  for(let i=1; i<count; i++){
     let newForm = `
-<div class="control demand-control">
-<input name="dj[d${count}]" class="input" type="text" placeholder="D${count}">
-</div>
-`;
-    form.innerHTML += newForm;
+    <div class="control demand-control">
+      <input name="dj[d${i}]" class="input" type="text" placeholder="D${i}">
+    </div>
+  `
+  	form.innerHTML += newForm;
+  }
 });
+
+/* buttonAdd.addEventListener('click', () => {
+  count +=1;
+  let newForm = `
+  <div class="control demand-control">
+    <input name="dj[d${count}]" class="input" type="text" placeholder="D${count}">
+  </div>
+`
+  form.innerHTML += newForm;
+}); */
